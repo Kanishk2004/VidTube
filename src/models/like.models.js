dda3c378361f87e8521bfe69f63ebc7af81d1942
Kanishk <67541211+Kanishk2004@users.noreply.github.com>
2024-07-04 18:28:39 +0530

    all models created

---
+++ src/models/like.models.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new Schema(
+	{
+		video: {
+			type: Schema.Types.ObjectId,
+			ref: "Video",
+		},
+		likedBy: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+		},
+		comment: {
+			type: Schema.Types.ObjectId,
+			ref: "Comment",
+		},
+		tweet: {
+			type: Schema.Types.ObjectId,
+			ref: "Tweet",
+		},
+	},
+	{ timestamps: true }
+);
+
+export const Like = mongoose.model("Like", likeSchema);
