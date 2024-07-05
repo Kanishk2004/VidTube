969dc3b68e2778f22f989bab45c2c38fdc5bae9d
Kanishk <67541211+Kanishk2004@users.noreply.github.com>
2024-07-05 14:14:38 +0530

    tweet controllers and routes completed and tested

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -6,16 +6,15 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { AsyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = AsyncHandler(async (req, res) => {
-	//TODO: create tweet
-
 	//Get tweet content from req.body
-	//check if the content isn't empty
-	//get owner from req.user coming from verifyJWT middleware
-	//save in db
+	//validation content shouldn't be empty
+	//get owner objectId from req.user coming from verifyJWT middleware
+	//create tweet in db
+	//return tweet as response
 
 	const { content } = req.body;
 
-	if (content?.trim() === "") {
+	if (!content || content?.trim() === "") {
 		throw new ApiError(400, "Content is required");
 	}
 
@@ -24,8 +23,6 @@ const createTweet = AsyncHandler(async (req, res) => {
 			$match: { _id: new mongoose.Types.ObjectId(req.user._id) },
 		},
 	]);
-
-	console.log(user);
 
 	const tweet = await Tweet.create({
 		content,
@@ -39,16 +36,104 @@ const createTweet = AsyncHandler(async (req, res) => {
 	return res.status(201).json(new ApiResponse(201, tweet, "Tweet created successfully"));
 });
 
+const getCurrentUserTweets = AsyncHandler(async (req, res) => {
+	const tweets = await Tweet.aggregate([
+		{
+			$match: {
+				owner: new mongoose.Types.ObjectId(req.user._id),
+			},
+		},
+	]);
+
+	return res.status(200).json(new ApiResponse(200, tweets, "Current user tweets fetched successfully"));
+});
+
 const getUserTweets = AsyncHandler(async (req, res) => {
-	// TODO: get user tweets
+	const { userId } = req.params;
+
+	const tweets = await Tweet.aggregate([
+		{
+			$match: {
+				owner: new mongoose.Types.ObjectId(userId),
+			},
+		},
+	]);
+
+	return res.status(200).json(new ApiResponse(200, tweets, "User tweets fetched successfully"));
 });
 
 const updateTweet = AsyncHandler(async (req, res) => {
 	//TODO: update tweet
+
+	//get the tweetId from req.params
+	//get the new content from req.body
+	//validate the content isn't empty
+	//validate that the user is updating its own tweet or other's
+	//update the tweet in db
+	//return the new tweet as response
+
+	const { tweetId } = req.params;
+	const { content } = req.body;
+
+	if (!content || content?.trim() === "") {
+		throw new ApiError(400, "Content must not be empty");
+	}
+
+	//finding if the given tweet id belongs to the logged in person
+	const tweets = await Tweet.aggregate([
+		{
+			$match: {
+				owner: new mongoose.Types.ObjectId(req.user._id),
+			},
+		},
+		{
+			$match: {
+				_id: new mongoose.Types.ObjectId(tweetId),
+			},
+		},
+	]);
+
+	if (Array.isArray(tweets) && tweets.length === 0) {
+		throw new ApiError(400, "Only the tweet owner is allowed to edit the tweet");
+	}
+
+	const updatedTweet = await Tweet.findByIdAndUpdate(
+		tweetId,
+		{
+			$set: { content },
+		},
+		{
+			new: true,
+		}
+	);
+
+	return res.status(200).json(new ApiResponse(200, updatedTweet, "Tweet updated successfully"));
 });
 
 const deleteTweet = AsyncHandler(async (req, res) => {
-	//TODO: delete tweet
+	const { tweetId } = req.params;
+
+	//finding if the given tweet id belongs to the logged in user or not
+	const tweets = await Tweet.aggregate([
+		{
+			$match: {
+				owner: new mongoose.Types.ObjectId(req.user._id),
+			},
+		},
+		{
+			$match: {
+				_id: new mongoose.Types.ObjectId(tweetId),
+			},
+		},
+	]);
+
+	if (Array.isArray(tweets) && tweets.length === 0) {
+		throw new ApiError(400, "Only the tweet owner is allowed to delete the tweet");
+	}
+
+	await Tweet.findByIdAndDelete(tweetId);
+
+	return res.status(200).json(new ApiResponse(200, "Tweet deleted successfully", "Success"));
 });
 
-export { createTweet, getUserTweets, updateTweet, deleteTweet };
+export { createTweet, getCurrentUserTweets, getUserTweets, updateTweet, deleteTweet };
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,12 +1,19 @@
 import { Router } from "express";
-import { createTweet, deleteTweet, getUserTweets, updateTweet } from "../controllers/tweet.controller.js";
+import {
+	createTweet,
+	deleteTweet,
+	getCurrentUserTweets,
+	getUserTweets,
+	updateTweet,
+} from "../controllers/tweet.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router.route("/").post(createTweet); //tested
-router.route("/user/:userId").get(getUserTweets);
-router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
+router.route("/user/:userId").get(getUserTweets); //tested
+router.route("/mytweets").get(getCurrentUserTweets); //tested
+router.route("/:tweetId").patch(updateTweet).delete(deleteTweet); //tested
 
 export default router;
