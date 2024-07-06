544dc8950f9b903d408d8f803e52eafb53ef2b42
Kanishk <67541211+Kanishk2004@users.noreply.github.com>
2024-07-06 15:17:58 +0530

    comment controller (without pagination)

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -7,19 +7,86 @@ import { AsyncHandler } from "../utils/asyncHandler.js";
 const getVideoComments = AsyncHandler(async (req, res) => {
 	//TODO: get all comments for a video
 	const { videoId } = req.params;
-	const { page = 1, limit = 10 } = req.query;
+	// const { page = 1, limit = 10 } = req.query;
+
+	const videoComments = await Comment.aggregate([
+		{
+			$match: { video: new mongoose.Types.ObjectId(videoId) },
+		},
+	]);
+
+	return res.status(200).json(new ApiResponse(200, videoComments, "Successfully fetched video comments"));
 });
 
 const addComment = AsyncHandler(async (req, res) => {
 	// TODO: add a comment to a video
+	const { videoId } = req.params;
+	const { content } = req.body;
+
+	if (!videoId) {
+		throw new ApiError(400, "Please provide valid video id");
+	}
+
+	if (!content.trim() === "") {
+		throw new ApiError(400, "Please provide valid content");
+	}
+
+	const comment = await Comment.create({
+		content,
+		video: new mongoose.Types.ObjectId(videoId),
+		owner: new mongoose.Types.ObjectId(req.user._id),
+	});
+
+	return res.status(200).json(new ApiResponse(200, comment, "Successfully posted a comment"));
 });
 
 const updateComment = AsyncHandler(async (req, res) => {
 	// TODO: update a comment
+	const { commentId } = req.params;
+	const { content } = req.body;
+
+	if (!commentId) {
+		throw new ApiError(400, "Please provide valid comment id");
+	}
+
+	const comment = await Comment.findById(commentId);
+
+	if (!(comment.owner.toString() === req.user._id.toString())) {
+		throw new ApiError(400, "Only owner can update the comment");
+	}
+
+	if (!content.trim() === "") {
+		throw new ApiError(400, "Please provide valid content");
+	}
+
+	const newComment = await Comment.findByIdAndUpdate(
+		commentId,
+		{
+			$set: { content: content },
+		},
+		{ new: true }
+	);
+
+	return res.status(200).json(new ApiResponse(200, newComment, "Successfully updated the comment."));
 });
 
 const deleteComment = AsyncHandler(async (req, res) => {
 	// TODO: delete a comment
+	const { commentId } = req.params;
+
+	if (!commentId) {
+		throw new ApiError(400, "Please provide valid comment id");
+	}
+
+	const comment = await Comment.findById(commentId);
+
+	if (!(comment.owner.toString() === req.user._id.toString())) {
+		throw new ApiError(400, "Only owner can update the comment");
+	}
+
+	await Comment.findByIdAndDelete(commentId);
+
+	return res.status(200).json(new ApiResponse(200, "Successfully deleted the comment", "success"));
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
